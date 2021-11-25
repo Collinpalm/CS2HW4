d247f038f6d71fcfd0a166ae4dc47f8252b21503
Collinpalm <collinpalm@gmail.com>
2021-11-25 15:07:40 -0500

    passes all cases and has all the necessary comments

--- src/sequencealignment/SequenceAlignment.java
+++ src/sequencealignment/SequenceAlignment.java
@@ -30,13 +30,15 @@ public class SequenceAlignment {
         a = new int[m+1][n+1];
         arrows = new char[m+1][n+1];
     }
-
+    //builds the matrix to get the proper alignment
     public void computeAlignment(int gencost){
-        int min, pos;
+        int min, pos;//var declarations
+        //fills in first column
         for(int i = 0;i<m;i++){
             a[i][0] = i*gencost;
             setArrow(1, i, 0);
         }
+        //fills in first row
         for(int j = 0;j<n;j++){
             a[0][j] = j*gencost;
             setArrow(2, 0, j);
@@ -47,35 +49,42 @@ public class SequenceAlignment {
                 arr[0] = alpha(one.charAt(i-1), two.charAt(j-1)) + a[i-1][j-1];
                 arr[1] = gencost + a[i-1][j];
                 arr[2] = gencost + a[i][j-1];
-                min = 0;
+                min = arr[0];
                 pos = 0;
                 for (int k = 0;k<3;k++){
-                    if(arr[k]>min) {
+                    if(arr[k]<min) {//picks the smallest value
                         min = arr[k];
                         pos = k;
                     }
                 }
-                a[i][j] = min;
-                setArrow(pos, i, j);
+                a[i][j] = min;//sets the number in the matrix
+                setArrow(pos, i, j);//sets the arrow in the matrix
             }
         }
     }
 
     public void setArrow(int num, int i, int j){
+        //picks which directions the arrow points based on the
+        //cases from class
         switch(num) {
             case 0:
                 this.arrows[i][j] = 'D';
+                //diagonal
                 break;
             case 1:
                 this.arrows[i][j] = 'U';
+                //vertical
                 break;
             case 2:
                 this.arrows[i][j] = 'R';
+                //horizontal
                 break;
         }
-
+        //yes I realize that the chars do not match the first letter of the words
+        //leave me alone
     }
-
+    //method to get the value for alpha, it checks if they are the same
+    //then if they are both a vowel or a consonant, otherwise returns the max value
     public int alpha(char a, char b){
         if(a == b){
             return 0;
@@ -88,18 +97,25 @@ public class SequenceAlignment {
         }
         return 3;
     }
-
+//called by the runner
     public String getAlignment(){
+
+
         StringBuilder str1 = new StringBuilder();
         StringBuilder str2 = new StringBuilder();
         buildAlign(m, n, str1, str2);
         return str1+" "+str2;
     }
-
+    //recursive function that builds the strings from back to front, inserting '-' where appropriate
+    /*I really thought this was very smart, I am using the stringbuilder objects to insert the new
+     *char at the front.  Of course, you could use a char arraylist for the same purpose, but I feel
+     *like if stringbuilder exists why not use it.
+    */
     public void buildAlign(int i, int j, StringBuilder str1, StringBuilder str2){
         if(i<=0 || j<= 0){
             return;
         }
+        //checks the key values and does the proper operations
         switch(this.arrows[i][j]){
             case 'D':
                 str1.insert(0, one.charAt(i-1));
