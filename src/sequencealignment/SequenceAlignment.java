261bd0ee96549e3f7f6ddb7a0055bad340cef6d8
Collinpalm <collinpalm@gmail.com>
2021-11-25 14:34:31 -0500

    it should be done

--- src/sequencealignment/SequenceAlignment.java
+++ src/sequencealignment/SequenceAlignment.java
@@ -1,4 +1,7 @@
 package sequencealignment;
+import java.util.*;
+import java.lang.*;
+
 /* Name:
  * Collin Palm
  * COP3503 Fall 2021
@@ -13,7 +16,7 @@ public class SequenceAlignment {
     private String one;
     private String two;
     private int[][] a;//matrix to compute the optimal cost
-    private int[][] arrows;//matrix to store the arrow directions
+    private char[][] arrows;//matrix to store the arrow directions
     int m;//stores length of string one, so I don't completely annihilate the runtime
     int n;//same thing as m but for string two
 
@@ -25,25 +28,91 @@ public class SequenceAlignment {
         m = one.length();
         n = two.length();
         a = new int[m][n];
-        arrows = new int[m][n];
+        arrows = new char[m][n];
     }
 
     public void computeAlignment(int gencost){
+        int min, pos;
         for(int i = 0;i<m;i++){
             a[i][0] = i*gencost;
+            setArrow(1, i, 0);
         }
         for(int j = 0;j<n;j++){
             a[0][j] = j*gencost;
+            setArrow(2, 0, j);
         }
         int[] arr = {0,0,0};//array to store the 3 options and pick the minimum
         for(int j = 0; j<n;j++){
             for(int i = 0; i<m; i++){
-
+                arr[0] = alpha(one.charAt(i), two.charAt(j)) + a[i-1][j-1];
+                arr[1] = gencost + a[i-1][j];
+                arr[2] = gencost + a[i][j-1];
+                min = 0;
+                pos = 0;
+                for (int k = 0;k<3;k++){
+                    if(arr[k]>min) {
+                        min = arr[k];
+                        pos = k;
+                    }
+                }
+                a[i][j] = min;
+                setArrow(pos, i, j);
             }
         }
     }
 
+    public void setArrow(int num, int i, int j){
+        switch(num) {
+            case 0:
+                this.arrows[i][j] = 'D';
+                break;
+            case 1:
+                this.arrows[i][j] = 'U';
+                break;
+            case 2:
+                this.arrows[i][j] = 'R';
+                break;
+        }
+
+    }
+
+    public int alpha(char a, char b){
+        if(a == b){
+            return 0;
+        }
+        if("aeiouy".contains(""+a) && "aeiouy".contains(""+b)){
+            return 1;
+        }
+        if(!"aeiouy".contains(""+a) && !"aeiouy".contains(""+b)){
+            return 1;
+        }
+        return 3;
+    }
+
     public String getAlignment(){
         return one;
+    }
+
+    public void buildAlign(int i, int j, StringBuilder str1, StringBuilder str2){
+        if(i<0 || j< 0){
+            return;
+        }
+        switch(this.arrows[i][j]){
+            case 'D':
+                str1.insert(0, one.charAt(i));
+                str2.insert(0, two.charAt(j));
+                buildAlign(i-1, j-1, str1, str2);
+                break;
+            case 'U':
+                str1.insert(0, one.charAt(i));
+                str2.insert(0, '-');
+                buildAlign(i-1, j, str1, str2);
+                break;
+            case 'R':
+                str1.insert(0, '-');
+                str2.insert(0, two.charAt(j));
+                buildAlign(i, j-1, str1, str2);
+                break;
+        }
     }
 }
