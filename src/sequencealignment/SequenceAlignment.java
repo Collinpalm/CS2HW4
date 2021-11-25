4cb5d19a5d4b0524ff22ecae61bfed6f06725ba0
Collinpalm <collinpalm@gmail.com>
2021-11-25 13:51:30 -0500

    working on the computeAlignment method

--- src/sequencealignment/SequenceAlignment.java
+++ src/sequencealignment/SequenceAlignment.java
@@ -1,4 +1,49 @@
 package sequencealignment;
+/* Name:
+ * Collin Palm
+ * COP3503 Fall 2021
+ * Programming Assignment 4
+ */
+
+
+
 
 public class SequenceAlignment {
+    //obj variables
+    private String one;
+    private String two;
+    private int[][] a;//matrix to compute the optimal cost
+    private int[][] arrows;//matrix to store the arrow directions
+    int m;//stores length of string one, so I don't completely annihilate the runtime
+    int n;//same thing as m but for string two
+
+    //constructor
+    public SequenceAlignment(String first, String second) {
+        one = first;
+        two = second;
+        //LOOK IM DOING A DYNAMIC PROGRAMING THING!
+        m = one.length();
+        n = two.length();
+        a = new int[m][n];
+        arrows = new int[m][n];
+    }
+
+    public void computeAlignment(int gencost){
+        for(int i = 0;i<m;i++){
+            a[i][0] = i*gencost;
+        }
+        for(int j = 0;j<n;j++){
+            a[0][j] = j*gencost;
+        }
+        int[] arr = {0,0,0};//array to store the 3 options and pick the minimum
+        for(int j = 0; j<n;j++){
+            for(int i = 0; i<m; i++){
+
+            }
+        }
+    }
+
+    public String getAlignment(){
+        return one;
+    }
 }
